b5cfae7868c258af247a3b9c3464cb6564c19b01
Phylumium0001 <phylumium3000@gmail.com>
2025-06-11 01:03:59 +0100

    Sessions tutorials

--- passportJsYt/sessions/sessions.js
+++ passportJsYt/sessions/sessions.js
@@ -1,34 +1,68 @@
 const express = require("express")
-const session = require("express-session")
+const crypto = require("crypto")
+
+const expressSessions = require("express-session")
+
+const pgSessions = require("connect-pg-simple")(expressSessions)
+const pg = require("pg")
 
 const app = express()
 
-app.use(express.urlencoded())
-
-// Defining options for session
-app.use(session({
-    secret: "secret animal",
-    cookie:{
-        maxAge : 60000,
-        httpOnly : false
-    },
-    resave:true,
-    saveUninitialized :true,
-}))
-
-
-app.get("/", (req, res,next)=>{
-    console.log(req.session)
-    console.log(req.session.cookie)
-    if (req.session.views){
-        req.session.views++
-        res.writeHead(200,"Content-Type", 'text/html')
-        console.log(req.session.views);
-        res.end()
-    }else{
-        req.session.views = 1
-    }
-    res.send("Hello Welcome.")
+//  Create connection to database
+const pgPool = new pg.Pool({
+    "host":"localhost",
+    "port":"5432",
+    "database":"postgres",
+    "user":"postgres",
+    "password":"Xenon_111"
 })
 
-app.listen(5000,()=>{"Server Initiated on 5000"})
+// Create channel to link the database to the express session
+const pgStore = new pgSessions({
+    "pool":pgPool,
+    "tableName":"session",
+    "createTableIfMissing":true
+})
+
+// function checkSessionId(req,res,next) {
+//     result = pgPool.query(`SELECT * WHERE sid=${id}`)
+//     if (result){
+//         console.log("Welcome");
+//     }else{
+//         console.log("New session");
+//     }
+//     next()
+// }
+
+app.use(expressSessions({
+    // Linked database to express sessions through store
+    store: pgStore,
+    name:"tutorial",
+    genid:()=>{return crypto.randomUUID()},
+    
+    // Setup cookie
+    cookie:{
+        maxAge: 10 * 60 * 60 * 100,// Hour * minutes * seconds * milliseconds
+        httpOnly:false
+    },
+
+    secret:"Naughty",
+    saveUninitialized:true,
+    resave:false
+}))
+
+app.get("/",(req,res)=>{
+    console.log(req.session);
+    // Add views
+    if (req.session.viewCount){
+        req.session.viewCount++
+    }else{
+        // First time accessing session
+        req.session.viewCount = 1
+    }
+    // res.send("Welcome to the Sessions Tutorials")
+    res.send(`You have visited ${req.session.viewCount} times`)
+})
+
+app.listen(5000, ()=>{console.log("Server Connected on port 5000");
+})
